82b71a158716da296c8d5f3c8f1f4da0e38f032c
Veronika <e-mail@mail.com>
2024-06-04 16:58:05 +0300

    DT-7: add tasks section

--- src/components/tasks/tasks.tsx
+++ src/components/tasks/tasks.tsx
@@ -4,9 +4,20 @@ import './tasks.scss';
 const Tasks = (): ReactElement => {
     return (
         <main className="main-content">
-        <section className={"hero"}>
-            <h2>this is the tasks page</h2>
-        </section>
+            <section className="tasks-section">
+                <h2>Tasks</h2>
+                <div className="tasks-container">
+                    <article className="task-article">
+                        <h3>Task 1</h3>
+                    </article>
+                    <article className="task-article">
+                        <h3>Task 2</h3>
+                    </article>
+                    <article className="task-article">
+                        <h3>Task 3</h3>
+                    </article>
+                </div>
+            </section>
         </main>
 
     );
