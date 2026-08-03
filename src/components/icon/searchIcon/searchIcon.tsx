cb4d04cdac91441e010bd5efa22936870e6a4212
Veronika <e-mail@mail.com>
2026-08-03 22:11:42 +0300

    feature/DT-37: added: filtering and sorting queries and task search reset feature

--- src/components/icon/searchIcon/searchIcon.tsx
+++ src/components/icon/searchIcon/searchIcon.tsx
@@ -2,15 +2,33 @@ import React from "react";
 
 import styles from './SearchIcon.module.scss';
 
-type SearchInputProps = {
-    handleSearchUpdate: () => void;
-};
+interface SearchInputProps {
+    hasCross: boolean;
+    onSearch: () => void;
+    onClear: () => void;
+}
 
-const SearchIcon: React.FC<SearchInputProps> = ({handleSearchUpdate}) => {
+const SearchIcon: React.FC<SearchInputProps> = ({ onSearch, hasCross, onClear}) => {
     return (
         <button
-            onClick={() => handleSearchUpdate()}
+            onClick={hasCross ? onClear : onSearch}
             className={styles.searchButton}>
+            {hasCross ? (
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    width="1.5em"
+                    height="1.5em"
+                    className={styles.searchIcon}
+                >
+                    <path
+                        fill="currentColor"
+                        fillRule="evenodd"
+                        d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41Z"
+                    />
+                </svg>
+            ) : (
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -24,7 +42,8 @@ const SearchIcon: React.FC<SearchInputProps> = ({handleSearchUpdate}) => {
                     fillRule="evenodd"
                     d="M11 4.5a6.5 6.5 0 1 1 0 13 6.5 6.5 0 0 1 0-13Zm0 1.3a5.2 5.2 0 1 0 0 10.4 5.2 5.2 0 0 0 0-10.4Zm6.24 9.69 3.19 3.2a.65.65 0 1 1-.92.92l-3.2-3.19a.65.65 0 0 1 .93-.93Z"
                 />
-            </svg>
+            </svg> )
+            }
         </button>
     )
 }
--- src/service/service.ts
+++ src/service/service.ts
@@ -15,52 +15,94 @@ export interface TasksData {
     number: number
 }
 
-interface TasksRequestBody {
+export interface FetchTasksParams {
+    currentPage: number;
+    pageSize: number;
+    difficulty?: string;
+    searchValue?: string;
+    sortName?: 'asc' | 'desc' | 'none';
+    sortDifficulty?: 'asc' | 'desc' | 'none';
+    sortSolutions?: 'asc' | 'desc' | 'none';
+}
+
+type SortFieldName = "TITLE" | "DIFFICULTY" | "NUMBER_OF_SOLUTIONS";
+type SortDirection = "ASC" | "DESC";
+
+export interface SortField {
+    filedName: SortFieldName;
+    direction: SortDirection;
+}
+
+export interface TasksRequestBody {
     page: number,
     size: number,
     difficulty?: string,
     title?: string,
-    numberOfSolutions?: number
+    numberOfSolutions?: number,
+    sortFiled?: SortField[];
 }
 
 const getTasks = createAsyncThunk<
     TasksData,
-    TasksRequestBody & { url: string },
+    FetchTasksParams,
     { rejectValue: string }
 >(
     'tasksData/fetchTasks',
-    async ({
-               url,
-               page,
-               size,
-               difficulty,
-               title,
-               numberOfSolutions
-           },
-           {rejectWithValue}
-    ) => {
+    async (params, {rejectWithValue}) => {
+    const {
+            currentPage,
+            pageSize,
+            difficulty,
+            searchValue,
+            sortName,
+            sortDifficulty,
+            sortSolutions,
+        } = params;
         try {
             let tasksRequestBody: TasksRequestBody = {
-                page: page,
-                size: size
+                page: currentPage,
+                size: pageSize
             };
 
             if (difficulty && difficulty !== 'all') {
                 tasksRequestBody.difficulty = difficulty.toUpperCase();
             }
 
-            if (title) {
-                tasksRequestBody.title = title;
+            if (searchValue) {
+                tasksRequestBody.title = searchValue;
             }
 
-            if (numberOfSolutions) {
-                tasksRequestBody.numberOfSolutions = numberOfSolutions;
+            const sortFiled: SortField[] = [];
+
+            if (sortName !== 'none') {
+                sortFiled.push({
+                    filedName: 'TITLE',
+                    direction: sortName === 'asc' ? 'ASC' : 'DESC',
+                });
+            }
+
+            if (sortDifficulty !== 'none') {
+                sortFiled.push({
+                    filedName: 'DIFFICULTY',
+                    direction: sortDifficulty === 'asc' ? 'ASC' : 'DESC',
+                });
+            }
+
+            if (sortSolutions !== 'none') {
+                sortFiled.push({
+                    filedName: 'NUMBER_OF_SOLUTIONS',
+                    direction: sortSolutions === 'asc' ? 'ASC' : 'DESC',
+                });
+            }
+
+            if (sortFiled.length > 0) {
+                tasksRequestBody.sortFiled = sortFiled;
             }
 
             const response: AxiosResponse<TasksData> = await axios.post<TasksData>(
-                url,
+                'https://localhost:8443/api/v1/tasks',
                 tasksRequestBody,
-                { withCredentials: true }
+                {withCredentials: true}
             );
 
             return response.data;
